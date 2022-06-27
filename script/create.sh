a626fc5ff4dadac85443ca4d92bae4cbafbbabf9
Jim Xu <jxu@mentortek.com>
2022-06-27 12:17:14 -0500

    2.0.4 CodeDeploy kubectl apply - WORKS

--- script/create.sh
+++ script/create.sh
@@ -2,4 +2,6 @@
 #
 echo "CodeDeploy create.sh"
 #
-/usr/local/bin/kubectl apply -f /var/tmp/eks/hello-k8s.yml
+# /usr/local/bin/kubectl apply -f /var/tmp/eks/hello-k8s.yml
+/usr/local/bin/kubectl apply -f /var/tmp/eks/replicaset.yml
+
